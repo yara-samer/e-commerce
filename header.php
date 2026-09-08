de5d95ec006c919dcf5f2db760e453f69ac0626e
Rahma-Mostafa186 <rahma.mostafa186k@gmail.com>
2026-09-09 00:33:15 +0300

    upload full project code

---
+++ header.php
@@ -0,0 +1,30 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Studentia</title>
+    <link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+</head>
+<body>
+    <div class="header">
+        <div class="left">
+            <img src="Studentia.png" alt="Studentia Logo"><br>
+            <h1>Studentia</h1>
+        </div>
+        <form  class="bar">
+            <ul>
+                <li><a href="home.php">Home</a></li>
+                <li><a href="Properities.php">Housing</a></li>
+                <li><a href="profile.php">Profile</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="about.php">About</a></li>
+            </ul>
+        </form>
+        <div class="right">
+            <button>Log In</button>
+            <button>Sign Up</button>
+        </div>
+    </div>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php include("header.php") ?>
+<div class="container"></div>
+<?php include("footer.php") ?>
